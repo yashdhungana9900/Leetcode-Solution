61778f80018ce9e8fc7783d4f39cbdc65ae51df8
Yashraj <yashdhungana9900@gmail.com>
2026-08-08 00:24:03 +0530

    linked list sol.

---
+++ Linked_List.py/143_Reorder_List.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def reorderList(self, head):
+
+        if not head or not head.next:
+            return
+
+        # Step 1: Find middle
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse second half
+        prev = None
+        curr = slow.next
+        slow.next = None
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        # Step 3: Merge
+        first = head
+        second = prev
+
+        while second:
+
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
