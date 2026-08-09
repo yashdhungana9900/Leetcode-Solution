fbc115cacb1cdc0dde91a92e9c321e86d1c2f23c
Yashraj <yashdhungana9900@gmail.com>
2026-08-09 18:19:16 +0530

    Add stack solution

---
+++ Stack/844_backspace_compare.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def backspaceCompare(self, s, t):
+
+        def build(string):
+            stack = []
+
+            for ch in string:
+                if ch == "#":
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(ch)
+
+            return stack
+
+        return build(s) == build(t)
