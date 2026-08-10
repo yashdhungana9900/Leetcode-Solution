740c11f1631662cc6483fcafd5c523aad6a53205
Yashraj <yashdhungana9900@gmail.com>
2026-08-10 18:45:41 +0530

    Add stack solution

---
+++ Stack/225_implementing_stack_using_Queue.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class MyStack(object):
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x):
+        self.q.append(x)
+
+        # Move previous elements behind x
+        for i in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self):
+        return self.q.popleft()
+
+    def top(self):
+        return self.q[0]
+
+    def empty(self):
+        return len(self.q) == 0
