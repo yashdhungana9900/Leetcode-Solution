dfdbd9c3e5b487b7fc0d711e8972b08ae63ab4ae
Yashraj <yashdhungana9900@gmail.com>
2026-08-15 19:58:52 +0530

    Add array solution

---
+++ Arrays/69_sqr(x).py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        left = 0
+        right = x
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            if mid * mid == x:
+                return mid
+
+            elif mid * mid < x:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return right
+        
