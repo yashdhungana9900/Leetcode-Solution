accd15643b773a4c2500b053705cf49cf8631a48
Yashraj <yashdhungana9900@gmail.com>
2026-08-18 22:25:00 +0530

    Add Two Pointer solution

---
+++ Two_Pointers/680_valid_Palindrom2.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def validPalindrome(self, s):
+
+        def isPalindrome(left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+
+            return True
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+
+            if s[left] != s[right]:
+
+                return (isPalindrome(left + 1, right) or
+                        isPalindrome(left, right - 1))
+
+            left += 1
+            right -= 1
+
+        return True
