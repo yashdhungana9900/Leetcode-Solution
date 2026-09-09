83873bdef48cd8e2895965e52f2bbb8e2a1017dc
Yashraj <yashdhungana9900@gmail.com>
2026-09-09 13:06:14 +0530

    Add tree solution

---
+++ Trees/104_max_depth_in_binary_tree.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maxDepth(self, root):
+        if not root:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left, right)
